a01c83ee1adef373c786259214fa1f4065b3c21e
CrysPy-SAM <ssmishra9918@gmail.com>
2025-11-18 20:35:16 +0530

    Initial commit: added solution and test cases

--- solution.js
+++ solution.js
@@ -47,16 +47,7 @@ function solution(D) {
   for (let i = first; i <= last; i++) {
     if (result[week[i]] === null) {
       result[week[i]] = Math.round(firstVal + diff * (i - first));
-      class Solution {
-    public boolean isOneBitCharacter(int[] bits) {
-        int n = bits.length;
-        int i = 0;
-        while(i < n - 1)
-            i += bits[i] + 1;
-        return i == n - 1;
-    }
-}
-
+      
     }
   }
 
